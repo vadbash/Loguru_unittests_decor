684e43cca48feb789473aa70ffb40050c3d88339
vadbash <vadusikvadim@gmail.com>
2023-03-27 22:14:19 +0300

    loguru

--- log.py
+++ log.py
@@ -2,8 +2,10 @@ from loguru import logger
 import time
 import sys
 
+#main func
 def calc(a, b):
     result = a + b
+    #output
     logger.info("Params {} + {} = {}".format(a, b, result))
     start = time.time()
     end = time.time()
@@ -11,6 +13,7 @@ def calc(a, b):
     logger.info("Time = {}".format(prog_time))
     return result
 
+#logger params
 logger.add("file.log", rotation="10 MB")
 calc(10, 20)
 
--- log_link.py
+++ log_link.py
@@ -2,15 +2,17 @@ import requests
 from loguru import logger
 import time
 
-
+#finish output
 def link_site(link):
     url = f"{link}"
     res = requests.get(url)
     logger.info(f"Link = {link}, response = {res}")
     return res
 
+#logger params
 logger.add("file.log", rotation="10 MB")
 start = time.time()
+#test link
 link_site("https://github.com/")
 end = time.time()
 prog_time = end - start
