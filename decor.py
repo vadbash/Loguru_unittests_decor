c6f492ffde9b9e2b468020b5ee7e815003a0b148
vadbash <vadusikvadim@gmail.com>
2023-03-27 21:57:28 +0300

    decorator

---
+++ decor.py
@@ -0,0 +1,21 @@
+import time
+
+#main func
+def logging(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        res = func(*args, **kwargs)
+        end = time.time()
+        #time params
+        prog_time = end - start
+        #finish input
+        print(f"Time = {round(prog_time, 10)} seconds")
+        return res
+    return wrapper
+
+#decorator
+@logging
+def calc():
+    print("hello hello")
+calc()
+
