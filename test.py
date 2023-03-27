e22bf6fa41a5f138e377619767f783e72bb21e11
vadbash <vadusikvadim@gmail.com>
2023-03-27 22:22:39 +0300

    unittest

--- test.py
+++ test.py
@@ -1,30 +1,33 @@
 import unittest
 
+#main func
 def convert(s):
     return s.split()
 convert("Hello world")
 
-class TestConvertStringToWordList(unittest.TestCase):
+#testcase
+class Test_convert(unittest.TestCase):
+    #test1
     def test_simple(self):
         res = convert("Hello world")
         fin = ["Hello", "world"]
         self.assertEqual(res, fin)
-
+    #test2
     def test_big_string(self):
         res = convert("Hello hello world")
         fin = ["Hello", "hello", "world"]
         self.assertEqual(res, fin)
-
+    #test3
     def test_solid_text(self):
         res = convert("helloworld")
         fin = ["helloworld"]
         self.assertEqual(res, fin)
-
+    #test4
     def test_spaces(self):
         res = convert("Hello   world")
         fin = ["Hello", "world"]
         self.assertEqual(res, fin)
         
-
+#running
 if __name__ == '__main__':
     unittest.main()
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,22 +1,26 @@
 import unittest
 
+#main func
 def mirror(text):
     return text == text[::-1]
 
+#testcase
 class TestMirror(unittest.TestCase):
+    #test1
     def test_palindrome(self):
         self.assertTrue(mirror("tat"))
         self.assertTrue(mirror("momom"))
-    
+    #test2
     def test_non_palindrome(self):
         self.assertFalse(mirror("hello"))
         self.assertFalse(mirror("world"))
-
+    #test3
     def test_one(self):
         self.assertTrue(mirror("a"))
         self.assertTrue(mirror("2"))
         self.assertTrue(mirror("="))
 
+#running
 if __name__ == '__main__':
     unittest.main()
 
